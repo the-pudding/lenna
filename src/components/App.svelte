6eb93a10993381db2515f96d5b3965a51c7620a2
Michelle McGhee <michmac202@gmail.com>
2021-08-26 12:36:43 -0500

    screenshots fly in

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -3,14 +3,17 @@
   import Footer from "$components/Footer.svelte";
   import loadPixels from "$utils/loadPixels.js";
   import Canvas from "./Canvas.svelte";
+  import Screenshots from "./Screenshots.svelte";
 
-  let pixels;
+  // let pixels;
 
-  onMount(async () => {
-    pixels = await loadPixels("assets/img/lenna-84.png");
-  });
+  // onMount(async () => {
+  //   pixels = await loadPixels("assets/img/lenna-84.png");
+  // });
 </script>
 
-{#if pixels}
+<Screenshots />
+
+<!-- {#if pixels}
   <Canvas {pixels} />
-{/if}
+{/if} -->
---
+++ src/components/Screenshots.svelte
@@ -0,0 +1,45 @@
+<script>
+  import { fly } from "svelte/transition";
+
+  let visible = false;
+  const onClick = () => {
+    visible = true;
+  };
+</script>
+
+<button on:click={onClick}>start</button>
+{#if visible}
+  {#each [1, 2, 3, 4] as i}
+    <img
+      src={`assets/img/screenshot${i}.png`}
+      alt="screenshot of lenna"
+      in:fly={{ y: 200, duration: 2000, delay: i * 150 }}
+    />
+  {/each}
+{/if}
+
+<style>
+  img {
+    width: 40%;
+    top: 20%;
+    border: 2px solid blueviolet;
+    position: absolute;
+  }
+
+  img:nth-child(1) {
+    left: 10%;
+    transform: rotate(3deg);
+  }
+  img:nth-child(2) {
+    left: 30%;
+    transform: rotate(-3deg);
+  }
+  img:nth-child(3) {
+    left: 50%;
+    transform: rotate(3deg);
+  }
+  img:nth-child(4) {
+    left: 70%;
+    transform: rotate(-3deg);
+  }
+</style>
