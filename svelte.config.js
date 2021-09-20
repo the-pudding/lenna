f83cb7387a6830c5cda5270af624b08636cc063d
Michelle McGhee <michmac202@gmail.com>
2021-09-20 17:00:27 -0500

    update github pages

--- svelte.config.js
+++ svelte.config.js
@@ -5,35 +5,37 @@ import dsv from "@rollup/plugin-dsv";
 import sveltePreprocess from "svelte-preprocess";
 import autoprefixer from "autoprefixer";
 
+const dev = process.env.NODE_ENV === "development";
+
 const preprocess = sveltePreprocess({
-	postcss: {
-		plugins: [autoprefixer]
-	}
+  postcss: {
+    plugins: [autoprefixer]
+  }
 });
 
 const config = {
-	preprocess,
-	kit: {
-		adapter: adapterStatic(),
-		target: "#svelte",
-		vite: {
-			resolve: {
-				alias: {
-					"$actions": path.resolve("./src/actions"),
-					"$components": path.resolve("./src/components"),
-					"$data": path.resolve("./src/data"),
-					"$stores": path.resolve("./src/stores"),
-					"$styles": path.resolve("./src/styles"),
-					"$svg": path.resolve("./src/svg"),
-					"$utils": path.resolve("./src/utils")
-				}
-			},
-			plugins: [
-				dsv(),
-				svg()
-			]
-		}
-	}
+  preprocess,
+  kit: {
+    adapter: adapterStatic(),
+    target: "#svelte",
+    vite: {
+      resolve: {
+        alias: {
+          $actions: path.resolve("./src/actions"),
+          $components: path.resolve("./src/components"),
+          $data: path.resolve("./src/data"),
+          $stores: path.resolve("./src/stores"),
+          $styles: path.resolve("./src/styles"),
+          $svg: path.resolve("./src/svg"),
+          $utils: path.resolve("./src/utils")
+        }
+      },
+      plugins: [dsv(), svg()],
+      paths: {
+        base: dev ? "" : "/lenna"
+      }
+    }
+  }
 };
 
 export default config;
