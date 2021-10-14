5a59f5030995f60ed0e08017f4b176c2804a98f8
Michelle McGhee <michmac202@gmail.com>
2021-10-14 17:45:37 -0500

    slightly bigger legend

--- src/components/Legend.svelte
+++ src/components/Legend.svelte
@@ -5,12 +5,18 @@
   export let x;
   export let y;
 
-  const size = 20;
+  const size = 25;
 </script>
 
 <g transform={`translate(${x}, ${y})`}>
   {#each _.keys(barColors) as legendItem, i}
     <rect height={size} width={size} fill={barColors[legendItem]} y={i * size * 1.5} />
-    <text x={size * 1.5} y={i * size * 1.5 + 13}>{legendItem}</text>
+    <text x={size * 1.5} y={i * size * 1.5 + 16}>{legendItem}</text>
   {/each}
 </g>
+
+<style>
+  text {
+    font-size: 14px;
+  }
+</style>
