8e8a3bf86cee2b3cabc024ebfd0f3dbb7db3f390
Michelle McGhee <michmac202@gmail.com>
2021-10-29 10:34:59 -0500

    social images fixed

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -21,7 +21,7 @@
   <meta property="og:type" content="article" />
   <meta property="og:locale" content="en_US" />
 
-  <meta property="og:image" content="{url}/static/assets/img/social/social-facebook.jpg" />
+  <meta property="og:image" content="{url}/assets/img/social/social-facebook.jpg" />
   <meta property="og:image:type" content="image/jpeg" />
   <meta property="og:image:width" content="1200" />
   <meta property="og:image:height" content="600" />
@@ -31,7 +31,7 @@
   <meta name="twitter:creator" content="@puddingviz" />
   <meta name="twitter:title" content={title} />
   <meta name="twitter:description" content={description} />
-  <meta name="twitter:image:src" content="{url}/static/assets/img/social/social-twitter.jpg" />
+  <meta name="twitter:image:src" content="{url}/assets/img/social/social-twitter.jpg" />
 
   <meta name="robots" content="max-image-preview:large" />
 
